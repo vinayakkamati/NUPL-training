c903d9215fcc7b09de147343d94b3e5247b582a4
Vinayak Kamati <ervinayakkamati@gmail.com>
2023-07-19 22:22:41 +0530

    [IF-220] feat: create project get request

--- src/main/java/com/neptune/taskmanagement/controller/ProjectController.java
+++ src/main/java/com/neptune/taskmanagement/controller/ProjectController.java
@@ -1,18 +1,26 @@
 package com.neptune.taskmanagement.controller;
 
 import com.neptune.taskmanagement.model.Project;
+import com.neptune.taskmanagement.service.IProjectService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(value = "/projects")
 public class ProjectController {
 
-    @GetMapping("/1")
-    public Project firstOne(){
-        return new Project("name", LocalDate.now());
+    private final IProjectService iProjectService;
+
+    public ProjectController(IProjectService iProjectService) {
+        this.iProjectService = iProjectService;
+    }
+
+    @GetMapping(value = "/{id}")
+    public Project findOne(@PathVariable Long id){
+        return iProjectService.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
--- src/test/java/com/neptune/taskmanagement/spring/TestLoader.java
+++ src/test/java/com/neptune/taskmanagement/spring/TestLoader.java
@@ -18,7 +18,7 @@ public class TestLoader implements ApplicationContextAware {
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        projectRepository.save(new Project(1L,"My project", LocalDate.now()));
+        projectRepository.save(new Project(1L,"My project1", LocalDate.now()));
         projectRepository.save(new Project(2L, "My project3", LocalDate.now()));
         projectRepository.save(new Project(3L,"My project2", LocalDate.now()));
         projectRepository.save(new Project(4L,"My project4", LocalDate.now()));
