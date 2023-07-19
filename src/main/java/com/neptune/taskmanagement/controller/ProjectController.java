d6ad87301232e701ca628109f775ff325cc89e16
Vinayak Kamati <ervinayakkamati@gmail.com>
2023-07-19 22:32:07 +0530

    [IF-220] feat: create save project request

--- src/main/java/com/neptune/taskmanagement/controller/ProjectController.java
+++ src/main/java/com/neptune/taskmanagement/controller/ProjectController.java
@@ -3,10 +3,7 @@ package com.neptune.taskmanagement.controller;
 import com.neptune.taskmanagement.model.Project;
 import com.neptune.taskmanagement.service.IProjectService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
@@ -22,5 +19,10 @@ public class ProjectController {
     @GetMapping(value = "/{id}")
     public Project findOne(@PathVariable Long id){
         return iProjectService.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping(value = "/create")
+    public void createProject(@RequestBody Project project){
+        iProjectService.saveProject(project);
     }
 }
