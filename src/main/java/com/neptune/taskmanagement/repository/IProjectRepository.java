fe13639dac885c071ad745082bb00f5fa81efabe
Vinayak Kamati <119689320+vinayakkamati@users.noreply.github.com>
2023-07-19 13:00:49 +0530

    Feature/if 220 persistence (#3)
    * [IF-220] feat: configure persistence with spring data jpa
    
    * [IF-220] feat: implement find project between date created range

--- src/main/java/com/neptune/taskmanagement/repository/IProjectRepository.java
+++ src/main/java/com/neptune/taskmanagement/repository/IProjectRepository.java
@@ -3,8 +3,12 @@ package com.neptune.taskmanagement.repository;
 import com.neptune.taskmanagement.model.Project;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface IProjectRepository extends CrudRepository<Project, Long> {
     Optional<Project> findByName(String name);
+
+    List<Project> findByDateCreatedBetween(LocalDate start, LocalDate end);
 }
